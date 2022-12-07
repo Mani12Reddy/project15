0987e5f7fbfe98145f9d8ae343d69621bca3aed0
Mani12Reddy <reddymani2000@gmail.com>
2022-12-07 16:31:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdb5(request):
+    return render(request,'mdb5.html')
+
+
+def mdb4(request):
+    return render(request,'mdb4.html')
